6ecff729c0de61a456cef483c74cba8774a6f850
Wilhelm Medetz <wilhelm.medetz@simcontrol.com>
2016-07-21 14:32:19 +0200

    Item & project templates updates

--- Templates.Build.ps1
+++ Templates.Build.ps1
@@ -11,25 +11,34 @@ function RemoveItems($items)
     }
 }   
 
-gci *\*.vstemplate | foreach-object { 
-    $t = $_.BaseName
+function CreateTemplates($templateType)
+{
+    gci *\*.vstemplate | foreach-object
+    { 
+        $t = $_.BaseName
 
-    $temp =  (Resolve-Path "ProjectTemplates").Path + "\" + $t
-    Write-Host $temp
+        $temp = (Resolve-Path $templateType).Path + "\" + $t
+        Write-Host $temp
 
-    RemoveItems $temp, ("ProjectTemplates\"+$t+".zip")
+		New-Item -ItemType Directory -Force -Path $temp
+        RemoveItems $temp, ($templateType+"\"+$t+".zip")
     
-    Copy-Item -Path $t -Destination "ProjectTemplates" -Recurse
-    RemoveItems ($temp+"\bin"), ($temp+"\obj"), ($temp+"\"+$t+".csproj.user")
-    Get-ChildItem $temp -Recurse -include "*.cs", "*.csproj" | 
-        foreach-object {  $a = $_.fullname; (get-content $a) | 
-            foreach-object { $_ -replace $t, '$safeprojectname$' } |
-                set-content $a 
-        }
+        Copy-Item -Path $t -Destination $templateType -Recurse
+        RemoveItems ($temp+"\bin"), ($temp+"\obj"), ($temp+"\"+$t+".csproj.user")
+        Get-ChildItem $temp -Recurse -include "*.cs", "*.csproj" | 
+            foreach-object {  $a = $_.fullname; (get-content $a) | 
+                foreach-object { $_ -replace $t, '$safeprojectname$' } |
+                    set-content $a 
+            }
 
-    [System.IO.Compression.ZipFile]::CreateFromDirectory($temp, $temp+".zip", [System.IO.Compression.CompressionLevel]::Optimal, $false )
+        [System.IO.Compression.ZipFile]::CreateFromDirectory($temp, $temp+".zip", [System.IO.Compression.CompressionLevel]::Optimal, $false )
 
-    RemoveItems $temp
+        RemoveItems $temp
+    }
 }
+
+
+CreateTemplate "ItemTemplates"
+CreateTemplate "ProjectTemplates"
 
 Write-Host "Press 'Enter' to continue ..."; Read-Host
