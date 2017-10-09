686f890b1d4aed87f82e251c93ff6f273a2ccd35
Wilhelm Medetz <wilhelm.medetz@simcontrol.com>
2017-10-09 12:04:56 +0200

    NuGet package references update

--- SimControl.Samples.CSharp.ClassLibrary.Tests/ComponentTests.cs
+++ SimControl.Samples.CSharp.ClassLibrary.Tests/ComponentTests.cs
@@ -1,110 +1,110 @@
-﻿// Copyright (c) SimControl e.U. - Wilhelm Medetz. See LICENSE.txt in the project root for more information.
+﻿//// Copyright (c) SimControl e.U. - Wilhelm Medetz. See LICENSE.txt in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition.Hosting;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using Autofac;
-using NUnit.Framework;
-using SimControl.Log;
-using SimControl.Samples.CSharp.ClassLibrary;
-using SimControl.Samples.CSharp.ClassLibrary.Component;
-using SimControl.TestUtils;
+//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel.Composition.Hosting;
+//using System.IO;
+//using System.Linq;
+//using System.Reflection;
+//using Autofac;
+//using NUnit.Framework;
+//using SimControl.Log;
+//using SimControl.Samples.CSharp.ClassLibrary;
+//using SimControl.Samples.CSharp.ClassLibrary.Component;
+//using SimControl.TestUtils;
 
-namespace SimControl.Samples.CSharp.ClassLibrary.Tests
-{
-    [Log]
-    [TestFixture]
-    public class ComponentLoaderTest : TestFrame
-    {
-        [Test]
-        public static void Test1()
-        {
-            var builder = new ContainerBuilder();
+//namespace SimControl.Samples.CSharp.ClassLibrary.Tests
+//{
+//    [Log]
+//    [TestFixture]
+//    public class ComponentLoaderTest : TestFrame
+//    {
+//        [Test]
+//        public static void Test1()
+//        {
+//            var builder = new ContainerBuilder();
 
-            builder.Register(c => new Counter()).As<Counter>().SingleInstance();
-            builder.Register(c => new Element(c.Resolve<Counter>(), "A")).As<IElement>();
-            builder.Register(c => new Component1(c.Resolve<IElement>())).As<IComponent>();
+//            builder.Register(c => new Counter()).As<Counter>().SingleInstance();
+//            builder.Register(c => new Element(c.Resolve<Counter>(), "A")).As<IElement>();
+//            builder.Register(c => new Component1(c.Resolve<IElement>())).As<IComponent>();
 
-            IContainer container = builder.Build();
+//            IContainer container = builder.Build();
 
-            using (var scope = container.BeginLifetimeScope())
-                Assert.That(scope.Resolve<IComponent>().ElementName, Is.EqualTo("Element.0.A"));
-        }
+//            using (var scope = container.BeginLifetimeScope())
+//                Assert.That(scope.Resolve<IComponent>().ElementName, Is.EqualTo("Element.0.A"));
+//        }
 
-        [Test]
-        public static void Test2()
-        {
-            var builder = new ContainerBuilder();
+//        [Test]
+//        public static void Test2()
+//        {
+//            var builder = new ContainerBuilder();
 
-            builder.Register(c => new Counter()).As<Counter>().SingleInstance();
-            builder.RegisterInstance("Text").Named<string>("Text").ExternallyOwned();
-            builder.Register(c => new Element(c.Resolve<Counter>(), "B")).As<IElement>();
-            builder.Register(c => new Component2(c.Resolve<IElement>(), c.ResolveNamed<string>("Text"))).As<IComponent>();
+//            builder.Register(c => new Counter()).As<Counter>().SingleInstance();
+//            builder.RegisterInstance("Text").Named<string>("Text").ExternallyOwned();
+//            builder.Register(c => new Element(c.Resolve<Counter>(), "B")).As<IElement>();
+//            builder.Register(c => new Component2(c.Resolve<IElement>(), c.ResolveNamed<string>("Text"))).As<IComponent>();
 
-            IContainer container = builder.Build();
+//            IContainer container = builder.Build();
 
-            using (var scope = container.BeginLifetimeScope())
-                Assert.That(scope.Resolve<IComponent>().ElementName, Is.EqualTo("Element.0.B.Text"));
-        }
+//            using (var scope = container.BeginLifetimeScope())
+//                Assert.That(scope.Resolve<IComponent>().ElementName, Is.EqualTo("Element.0.B.Text"));
+//        }
 
-        [Test]
-        public static void Test3()
-        {
-            var builder = new ContainerBuilder();
+//        [Test]
+//        public static void Test3()
+//        {
+//            var builder = new ContainerBuilder();
 
-            builder.Register(c => new Counter()).As<Counter>().SingleInstance();
-            builder.RegisterInstance("Text").Named<string>("Text").ExternallyOwned();
-            builder.Register(c => new Element(c.Resolve<Counter>(), "C")).As<IElement>();
-            builder.Register(c => new Component1(c.Resolve<IElement>())).As<Component1>();
-            builder.Register(c => new Component2(c.Resolve<IElement>(), c.ResolveNamed<string>("Text"))).As<Component2>();
+//            builder.Register(c => new Counter()).As<Counter>().SingleInstance();
+//            builder.RegisterInstance("Text").Named<string>("Text").ExternallyOwned();
+//            builder.Register(c => new Element(c.Resolve<Counter>(), "C")).As<IElement>();
+//            builder.Register(c => new Component1(c.Resolve<IElement>())).As<Component1>();
+//            builder.Register(c => new Component2(c.Resolve<IElement>(), c.ResolveNamed<string>("Text"))).As<Component2>();
 
-            IContainer container = builder.Build();
+//            IContainer container = builder.Build();
 
-            using (var scope = container.BeginLifetimeScope())
-            {
-                Assert.That(scope.Resolve<Component1>().ElementName, Is.EqualTo("Element.0.C"));
-                Assert.That(scope.Resolve<Component2>().ElementName, Is.EqualTo("Element.1.C.Text"));
-            }
-        }
+//            using (var scope = container.BeginLifetimeScope())
+//            {
+//                Assert.That(scope.Resolve<Component1>().ElementName, Is.EqualTo("Element.0.C"));
+//                Assert.That(scope.Resolve<Component2>().ElementName, Is.EqualTo("Element.1.C.Text"));
+//            }
+//        }
 
-        [Test]
-        public static void Test4()
-        {
-            var builder = new ContainerBuilder();
+//        [Test]
+//        public static void Test4()
+//        {
+//            var builder = new ContainerBuilder();
 
-            builder.Register(c => new Counter()).As<Counter>().SingleInstance();
-            builder.RegisterInstance("Text").Named<string>("Text").ExternallyOwned();
-            builder.Register(c => new Element(c.Resolve<Counter>(), "B")).As<IElement>();
-            builder.Register(c => new Component1(c.Resolve<IElement>())).As<IComponent>();
-            builder.Register(c => new Component2(c.Resolve<IElement>(), c.ResolveNamed<string>("Text"))).As<IComponent>();
+//            builder.Register(c => new Counter()).As<Counter>().SingleInstance();
+//            builder.RegisterInstance("Text").Named<string>("Text").ExternallyOwned();
+//            builder.Register(c => new Element(c.Resolve<Counter>(), "B")).As<IElement>();
+//            builder.Register(c => new Component1(c.Resolve<IElement>())).As<IComponent>();
+//            builder.Register(c => new Component2(c.Resolve<IElement>(), c.ResolveNamed<string>("Text"))).As<IComponent>();
 
-            IContainer container = builder.Build();
+//            IContainer container = builder.Build();
 
-            using (var scope = container.BeginLifetimeScope())
-            {
-                IEnumerable<IComponent> components = scope.Resolve<IEnumerable<IComponent>>();
-                Assert.That(components.Count, Is.EqualTo(2));
-                Assert.That(components.Where(t => t is Component1).Count, Is.EqualTo(1));
-                Assert.That(components.Where(t => t is Component2).Count, Is.EqualTo(1));
-            }
-        }
+//            using (var scope = container.BeginLifetimeScope())
+//            {
+//                IEnumerable<IComponent> components = scope.Resolve<IEnumerable<IComponent>>();
+//                Assert.That(components.Count, Is.EqualTo(2));
+//                Assert.That(components.Where(t => t is Component1).Count, Is.EqualTo(1));
+//                Assert.That(components.Where(t => t is Component2).Count, Is.EqualTo(1));
+//            }
+//        }
 
-        [Test]
-        public static void Test5()
-        {
-            var builder = new ContainerBuilder();
+//        [Test]
+//        public static void Test5()
+//        {
+//            var builder = new ContainerBuilder();
 
-            builder.RegisterType<Counter>().SingleInstance();
-            builder.Register(c => new Element(c.Resolve<Counter>(), "A")).As<IElement>();
-            builder.RegisterType<Component1>().As<IComponent>();
+//            builder.RegisterType<Counter>().SingleInstance();
+//            builder.Register(c => new Element(c.Resolve<Counter>(), "A")).As<IElement>();
+//            builder.RegisterType<Component1>().As<IComponent>();
 
-            IContainer container = builder.Build();
+//            IContainer container = builder.Build();
 
-            using (var scope = container.BeginLifetimeScope())
-                Assert.That(scope.Resolve<IComponent>().ElementName, Is.EqualTo("Element.0.A"));
-        }
-    }
-}
+//            using (var scope = container.BeginLifetimeScope())
+//                Assert.That(scope.Resolve<IComponent>().ElementName, Is.EqualTo("Element.0.A"));
+//        }
+//    }
+//}
--- SimControl.Samples.CSharp.ClassLibrary.Tests/PluginLoaderTests.cs
+++ SimControl.Samples.CSharp.ClassLibrary.Tests/PluginLoaderTests.cs
@@ -1,73 +1,73 @@
-﻿// Copyright (c) SimControl e.U. - Wilhelm Medetz. See LICENSE.txt in the project root for more information.
+﻿//// Copyright (c) SimControl e.U. - Wilhelm Medetz. See LICENSE.txt in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition.Hosting;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using Autofac;
-using Autofac.Integration.Mef;
-using NUnit.Framework;
-using SimControl.Log;
-using SimControl.Samples.CSharp.ClassLibrary;
-using SimControl.Samples.CSharp.ClassLibrary.Tests;
-using SimControl.Samples.CSharp.Mef.Contracts;
-using SimControl.Samples.CSharp.Mef.Plugin;
-using SimControl.TestUtils;
+//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel.Composition.Hosting;
+//using System.IO;
+//using System.Linq;
+//using System.Reflection;
+//using Autofac;
+//using Autofac.Integration.Mef;
+//using NUnit.Framework;
+//using SimControl.Log;
+//using SimControl.Samples.CSharp.ClassLibrary;
+//using SimControl.Samples.CSharp.ClassLibrary.Tests;
+//using SimControl.Samples.CSharp.Mef.Contracts;
+//using SimControl.Samples.CSharp.Mef.Plugin;
+//using SimControl.TestUtils;
 
-namespace SimControl.Samples.CSharp.ClassLibrary.Tests
-{
-    [Log]
-    [TestFixture]
-    public class PluginLoaderTest: TestFrame
-    {
-        [Test]
-        public static void PluginLoaderTests_MEFCompositionContainer_GetExportedValue_InstantiatesPlugin1Object()
-        {
-            IPlugin plugin1;
-            IResource resource;
+//namespace SimControl.Samples.CSharp.ClassLibrary.Tests
+//{
+//    [Log]
+//    [TestFixture]
+//    public class PluginLoaderTest: TestFrame
+//    {
+//        [Test]
+//        public static void PluginLoaderTests_MEFCompositionContainer_GetExportedValue_InstantiatesPlugin1Object()
+//        {
+//            IPlugin plugin1;
+//            IResource resource;
 
-            using (var catalog = new TypeCatalog(typeof(TestResource), typeof(Plugin1)))
-            using (var container = new CompositionContainer(catalog))
-            {
-                plugin1 = container.GetExportedValue<IPlugin>();
-                resource = container.GetExportedValue<IResource>();
-            }
+//            using (var catalog = new TypeCatalog(typeof(TestResource), typeof(Plugin1)))
+//            using (var container = new CompositionContainer(catalog))
+//            {
+//                plugin1 = container.GetExportedValue<IPlugin>();
+//                resource = container.GetExportedValue<IResource>();
+//            }
 
-            Assert.IsNotNull(plugin1);
-            Assert.IsNotNull(resource);
+//            Assert.IsNotNull(plugin1);
+//            Assert.IsNotNull(resource);
 
-            Assert.AreEqual(resource.ResourceName, plugin1.ResourceName());
-        }
+//            Assert.AreEqual(resource.ResourceName, plugin1.ResourceName());
+//        }
 
-        [Test]
-        public static void PluginLoaderTests_Plugin_ResourceName_ReturnsInstantiatedResource()
-        {
-            using (var aggregateCatalog = new AggregateCatalog())
-            using (var assemblyCatalog = new AssemblyCatalog(typeof(Resource).Assembly))
-            using (var directoryCatalog = new DirectoryCatalog(Path.GetDirectoryName(
-                typeof(PluginLoaderTest).Assembly.Location), "*.Plugin*.dll"))
-            {
-                aggregateCatalog.Catalogs.Add(assemblyCatalog);
-                aggregateCatalog.Catalogs.Add(directoryCatalog);
+//        [Test]
+//        public static void PluginLoaderTests_Plugin_ResourceName_ReturnsInstantiatedResource()
+//        {
+//            using (var aggregateCatalog = new AggregateCatalog())
+//            using (var assemblyCatalog = new AssemblyCatalog(typeof(Resource).Assembly))
+//            using (var directoryCatalog = new DirectoryCatalog(Path.GetDirectoryName(
+//                typeof(PluginLoaderTest).Assembly.Location), "*.Plugin*.dll"))
+//            {
+//                aggregateCatalog.Catalogs.Add(assemblyCatalog);
+//                aggregateCatalog.Catalogs.Add(directoryCatalog);
 
-                var builder = new ContainerBuilder();
-                builder.RegisterComposablePartCatalog(aggregateCatalog);
+//                var builder = new ContainerBuilder();
+//                builder.RegisterComposablePartCatalog(aggregateCatalog);
 
-                using (IContainer container = builder.Build())
-                {
-                    var plugins = container.Resolve<IEnumerable<IPlugin>>().ToArray();
-                    var resource = container.Resolve<IResource>();
+//                using (IContainer container = builder.Build())
+//                {
+//                    var plugins = container.Resolve<IEnumerable<IPlugin>>().ToArray();
+//                    var resource = container.Resolve<IResource>();
 
-                    Assert.AreEqual(2, plugins.Length);
-                    Assert.AreEqual(resource.ResourceName, plugins[0].ResourceName());
-                    Assert.AreEqual(resource.ResourceName, plugins[1].ResourceName());
-                }
-            }
-        }
+//                    Assert.AreEqual(2, plugins.Length);
+//                    Assert.AreEqual(resource.ResourceName, plugins[0].ResourceName());
+//                    Assert.AreEqual(resource.ResourceName, plugins[1].ResourceName());
+//                }
+//            }
+//        }
 
-        [Test]
-        public static void PluginLoaderTests_TestDirectory_Contains2Plugins() => Assert.AreEqual(2, Directory.GetFiles(TestContext.CurrentContext.TestDirectory, "SimControl.Samples.CSharp.Mef.Plugin?.dll").Length);
-    }
-}
+//        [Test]
+//        public static void PluginLoaderTests_TestDirectory_Contains2Plugins() => Assert.AreEqual(2, Directory.GetFiles(TestContext.CurrentContext.TestDirectory, "SimControl.Samples.CSharp.Mef.Plugin?.dll").Length);
+//    }
+//}
