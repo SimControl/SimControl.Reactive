c9470da18ba7e2f004a75c1272c7ea0b8466304f
Wilhelm Medetz <wilhelm.medetz@simcontrol.com>
2018-03-12 14:12:56 +0100

    NuGet package references updated, Code analysis rules fixed

---
+++ SimControl.Samples.CSharp.ClassLibrary.Tests/CopyFileTestAdaptertests.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) SimControl e.U. - Wilhelm Medetz. See LICENSE.txt in the project root for more information.
+
+using System;
+using NUnit.Framework;
+using SimControl.Log;
+using SimControl.TestUtils;
+
+namespace SimControl.Samples.CSharp.ClassLibrary.Tests
+{
+    [Log]
+    [TestFixture]
+    public class CopyFileTestAdapterTests : TestFrame
+    {
+        #region Test
+
+        [SetUp]
+        public new void SetUp() => RegisterTestAdapter(new CopyFileTestAdapter("NLog.config", "NLog2.config"));
+
+        #endregion
+
+        [Test]
+        public static void CopyFileTestAdapter() { }
+    }
+}
---
+++ SimControl.Samples.CSharp.ClassLibrary.Tests/TempFilesTestAdapterTests.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) SimControl e.U. - Wilhelm Medetz. See LICENSE.txt in the project root for more information.
+
+using System;
+using System.IO;
+using NUnit.Framework;
+using SimControl.Log;
+using SimControl.TestUtils;
+
+namespace SimControl.Samples.CSharp.ClassLibrary.Tests
+{
+    [Log]
+    [TestFixture]
+    public class TempFilesTestAdapterTests : TestFrame
+    {
+        #region Test
+
+        [SetUp]
+        public new void SetUp() => RegisterTestAdapter(new TempFilesTestAdapter("tempfile1", "tempfile2"));
+
+        #endregion
+
+        [Test]
+        public static void CopyFileTestAdapter() => File.Copy(
+            TestContext.CurrentContext.TestDirectory + "\\" + "NLog.config",
+            TestContext.CurrentContext.TestDirectory + "\\" + "tempfile2", true);
+    }
+}
