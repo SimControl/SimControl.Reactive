7f1eefaaff58db1b0ebe2fbb96a1153e7341c391
Wilhelm Medetz <wilhelm.medetz@simcontrol.com>
2019-06-26 10:36:38 +0200

    Convert to new Visual Studio project types

--- SimControl.Samples.CSharp.Wcf.Client/SampleServiceClient.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) SimControl e.U. - Wilhelm Medetz. See LICENSE.txt in the project root for more information.
-
-using SimControl.LogEx;
-
-namespace SimControl.Samples.CSharp.Wcf.Client.SampleServiceTap
-{
-    [Log]
-    public partial class SampleServiceClient { }
-}
