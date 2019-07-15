8685527c662347b6db4bc833276729e019d05c14
Wilhelm Medetz <wilhelm.medetz@simcontrol.com>
2019-07-15 10:19:39 +0200

    Convert to new Visual Studio project types

---
+++ SimControl.Samples.CSharp.TestsOld/MethodInfoAdviceTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using ArxOne.MrAdvice.Advice;
+using ArxOne.MrAdvice.Annotation;
+using NUnit.Framework;
+
+namespace SimControl.Samples.CSharp.TestsOld
+{
+    [AttributeUsage(AttributeTargets.Method), Priority(Priority)]
+    public class Advice1Attribute: Attribute, IMethodInfoAdvice
+    {
+        public void Advise(MethodInfoAdviceContext context)
+        {
+            if (MethodInfoAdviceTests.Expected != Priority)
+                throw new InvalidOperationException();
+
+            MethodInfoAdviceTests.Expected = Priority-1;
+        }
+
+        public const int Priority = 1;
+    }
+
+    [AttributeUsage(AttributeTargets.Method), Priority(Priority)]
+    public class Advice2Attribute: Attribute, IMethodInfoAdvice
+    {
+        public void Advise(MethodInfoAdviceContext context)
+        {
+            if (MethodInfoAdviceTests.Expected != Priority)
+                throw new InvalidOperationException();
+
+            MethodInfoAdviceTests.Expected = Priority-1;
+        }
+
+        public const int Priority = 2;
+    }
+
+    class ClassA
+    {
+        static ClassA() { }
+
+        public ClassA() { }
+
+        [Advice1, Advice2]
+        public static void Method() { }
+    }
+
+    class ClassB
+    {
+        static ClassB() { }
+
+        public ClassB() { }
+
+        [Advice2, Advice1]
+        public static void Method() { }
+    }
+
+    [TestFixture]
+    public class MethodInfoAdviceTests
+    {
+        [Test]
+        public void MethodInfoAdviceClassA_MethodA()
+        {
+            Expected = 2;
+            ClassA.Method();
+            Assert.That(Expected, Is.Zero);
+        }
+
+        [Test]
+        public void MethodInfoAdviceClassB_MethodB()
+        {
+            Expected = 2;
+            ClassB.Method(); // fails
+            Assert.That(Expected, Is.Zero);
+        }
+
+        public static int Expected;
+    }
+}
