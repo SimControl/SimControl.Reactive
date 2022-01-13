a68e300462d6306f08d5fdc2d542f73d00958848
Wilhelm Medetz <wilhelm.medetz@simcontrol.com>
2022-01-13 11:13:23 +0100

    StackOverflow samples

--- SimControl.Samples.CSharp.Tests/StackOverflowSamples.cs
+++ SimControl.Samples.CSharp.Tests/StackOverflowSamples.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) SimControl e.U. - Wilhelm Medetz. See LICENSE.txt in the project root for more information.
 
+using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using SimControl.Log;
@@ -20,5 +21,53 @@ namespace SimControl.Samples.CSharp.ClassLibrary.Tests
         {
             Assert.That(sense, Is.Not.EqualTo("eyes"));
         }
+    }
+
+    public static class LongTimeOperation
+    {
+        static LongTimeOperation()
+        {
+            Thread.Sleep(10000);
+            Resource = (++count).ToString();
+        }
+
+        public static string Resource { get; set; }
+        private static int count = 0;
+    }
+
+    [TestFixture]
+    public class TestFixture1
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            resource = LongTimeOperation.Resource;
+        }
+
+        [Test]
+        public void Test1() => Assert.That(resource, Is.EqualTo("1"));
+
+        [Test]
+        public void Test2() => Assert.That(resource, Is.EqualTo("1"));
+
+        private string resource;
+    }
+
+    [TestFixture]
+    public class TestFixture2
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            resource = LongTimeOperation.Resource;
+        }
+
+        [Test]
+        public void Test1() => Assert.That(resource, Is.EqualTo("1"));
+
+        [Test]
+        public void Test2() => Assert.That(resource, Is.EqualTo("1"));
+
+        private string resource;
     }
 }
