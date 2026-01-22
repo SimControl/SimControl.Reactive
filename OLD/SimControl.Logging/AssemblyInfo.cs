b5e775df464499489ff8d3bc65f1930e617d8faf
Wilhelm Medetz <wilhelm.medetz@simcontrol.com>
2026-01-22 09:52:44 +0100

    cleanup

---
+++ OLD/SimControl.Logging/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) SimControl e.U. - Wilhelm Medetz. See LICENSE.txt in the project root for more information.
+
+using System;
+
+[assembly: CLSCompliant(true)]
+
+// TODO project CR
